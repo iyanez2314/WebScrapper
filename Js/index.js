bf3cbea0ee1cf61e95528879114c733ab33181bf
Isaac Yanez <isaac231467@gmail.com>
2022-12-08 13:40:56 -0600

    messing with puppteer to take a screen shot of the page we are scraping MAYBE than can be easier than actually getting the element and then getting the value for each element

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,31 @@
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
 
-const url = 'https://www.ssense.com/en-us/men/clothing';
+const url = 'https://careers.microsoft.com/us/en/search-results?keywords=software';
+
+async function scraper(){
+    // this will launch a headless version chrome
+    const browser = await puppeteer.launch();
+
+    // open a new page in the browser
+    const page = await browser.newPage();
+
+    // this will go to the url we want to scrape
+    await page.goto(url);
+
+
+    await page.screenshot({
+        path: "./screenshot.png",
+        fullPage: true
+    });
+
+    await page.close();
+
+    await browser.close();
+};
+
+scraper();
 
 
